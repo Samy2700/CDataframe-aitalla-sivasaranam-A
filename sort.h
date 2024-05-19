c4d9d61ad0d900d0c90535bd37938ac2f5cd7a91
sssajin <151762061+sssajin@users.noreply.github.com>
2024-05-19 11:05:16 +0200

    Update sort.h
    sort.h

--- sort.h
+++ sort.h
@@ -1,8 +1,19 @@
-//
-// Created by samya on 09/05/2024.
-//
+#ifndef SORT_H
+#define SORT_H
 
-#ifndef CDATAFRAME_SORT_H
-#define CDATAFRAME_SORT_H
+#include "column.h"
 
-#endif //CDATAFRAME_SORT_H
+
+
+void sort_column(COLUMN col, int sort_dir);
+void print_col_by_index(COLUMNcol);
+void erase_index(COLUMN col);
+int check_index(COLUMNcol);
+void update_index(COLUMN col);
+int search_value_in_column(COLUMNcol, void val);
+
+void swap_indexes(unsigned long longa, unsigned long long b);
+void quick_sort(unsigned long longindex, int low, int high, int sort_dir, COLUMN col);
+int partition(unsigned long longindex, int low, int high, int sort_dir, COLUMN *col);
+
+#endif // SORT_H
